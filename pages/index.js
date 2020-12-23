88594507130dea327882d0aa86a721860a417cf1
Shaine Leibowitz <shainedl@gmail.com>
2020-12-22 22:03:31 -0700

    installs and change home page

--- pages/index.js
+++ pages/index.js
@@ -13,26 +13,6 @@ export default function Home({ allPostsData }) {
       </Head>
         <h2 className={utilStyles.heading2Xl}>{siteTitle}</h2>
         <h2 className={utilStyles.headingXl}>Data Scientist</h2>
-      <div className={styles.backToHome}>
-          <Link href="/projects">
-            <a>Check out some of my projects</a>
-          </Link>
-      </div>
-      <div className={styles.backToHome}>
-          <Link href="/posts">
-            <a>Read some of my blog posts</a>
-          </Link>
-      </div>
-      <div className={styles.backToHome}>
-          <a href='/data/ShaineLeibowitz_Resume.pdf'>Take a look at my Resume</a>
-      </div>     
-      <div className={styles.backToHome}>
-        <a href="https://github.com/shainedl">Go to my GitHub page</a>
-      </div> 
-      <div className={styles.backToHome}>
-          <a href="https://www.linkedin.com/in/shaine-leibowitz/">Connect with me on LinkedIn</a>
-      </div>
-      <div className={styles.backToHome}>Email me at ShaineDL@gmail.com</div>
       <div className={styles.backToHome}>Website designed and built by me using Next.js</div>
     </Layout>
   )
